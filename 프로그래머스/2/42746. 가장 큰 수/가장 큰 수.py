213bc9c81656af0f10655157aeb6dec6befe53e8
Hayden-yoonji <83634047+Hayden-yoonji@users.noreply.github.com>
2026-04-29 20:47:08 +0900

    [level 2] Title: 가장 큰 수, Time: 57.13 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,9 +1,22 @@
+# def solution(numbers):
+#     numbers = list(map(str,numbers))
+#     numbers.sort(key=lambda x:x*3, reverse=True)
+#     print(numbers)
+#     answer = ''.join(numbers)
+
+#     if answer[0] == '0':
+#         return "0"
+
+#     return answer
+
+
 def solution(numbers):
-    numbers = list(map(str,numbers))
+    numbers = list(map(str, numbers))
     numbers.sort(key=lambda x:x*3, reverse=True)
+
     answer = ''.join(numbers)
-
+    
     if answer[0] == '0':
-        return "0"
-
+        return '0'
+    
     return answer
