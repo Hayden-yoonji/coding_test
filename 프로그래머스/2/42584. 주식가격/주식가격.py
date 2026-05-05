968d232704c16b782029d1cce701163797693a1c
Hayden-yoonji <83634047+Hayden-yoonji@users.noreply.github.com>
2026-05-05 14:09:56 +0900

    [level 2] Title: 주식가격, Time: 68.69 ms, Memory: 18.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+def solution(prices):
+    answer = []
+    q = deque(prices)
+    while q:
+        stock = q.popleft()
+        cnt = 0
+        for i in q:
+            cnt +=1
+            if stock>i:
+                break
+        answer.append(cnt)
+    return answer
